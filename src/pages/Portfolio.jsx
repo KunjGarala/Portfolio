6defa6fa6aa5e3dc8e87c6eaa61fd7587dfde02c
KunjGarala <kunjgarala55@gmail.com>
2025-03-20 18:41:14 +0530

    project++

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom"
 import { Github, ExternalLink, Code2, Brain } from "lucide-react"
 import Spotifyimg from "../assets/Soptify.png"
+import Farmease from "../assets/Farmease.png"
 import LeetCodeProgress from "../components/LeetCodeProgress"
 
 const projects = [
   {
     id: 1,
+    title: "FarmEase",
+    description: " Built the frontend for a farmer-centric e-commerce platform, integrated APIs, and optimized performance for a smoother user experience.",
+    image: Farmease,
+    link: "https://drive.google.com/drive/folders/1AAaGoQ97zfx1rsNbOew6kkPtSICp0bGI",
+    github: "https://github.com/KunjGarala/FarmEase/tree/kunj-garala",
+    tags: ["React.js", " Tailwind CSS"]
+  },
+  {
+    id: 2,
     title: "Spotify Clone",
     description: "Developed a Spotify clone with an intuitive UI, dynamic volume control, and a responsive design for seamless music playback.",
     image: Spotifyimg,
